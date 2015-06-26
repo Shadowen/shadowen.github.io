da754b5109d8a3c88e6f6a3e96192c5e63d54399
wesleyOnLucy <wesley@heungs.com>
2015-06-26 14:51:39 -0400

    Added an attribute to <header> to allow pages to indicate which item should be active. 404 page now uses an error banner to indicate the problem.

--- header.js
+++ header.js
@@ -1,3 +1,5 @@
-$(function() {
-    $("header").load("/header.html");
+$(function () {
+    $("header").load("/header.html", function () {
+        $("header > nav > div :contains('" + $("header").attr("active") + "')").last().parent().attr("class", "active");
+    });
 });

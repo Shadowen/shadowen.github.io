22654ddd44d498f677e3aca4e191fdd2d87fd4cb
wesleyOnLucy <wesley@heungs.com>
2015-07-03 14:28:46 -0400

    Fixed the navbar-anchor conflict issue by adding a few lines of JavaScript to header.js. Added some writing to website.html

--- js/header.js
+++ js/header.js
@@ -1,8 +1,13 @@
 /* On document load */
-$(function() {
+$(function () {
     /* Load the header HTML */
-    $("header").load("/header.html", function() {
+    $("header").load("/header.html", function () {
         /* Set the active item on the navbar to to the one specified */
         $("#" + $("header").attr("active")).attr("class", "active");
     });
+    /* Fix anchors */
+    var shiftWindow = function () { scrollBy(0, -50); };
+    window.onhashchange = shiftWindow;
+//    window.addEventListener("hashchange", shiftWindow);
+    if (window.location.hash) shiftWindow();
 });

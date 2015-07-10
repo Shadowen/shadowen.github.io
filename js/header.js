2d2612e88dec32f76b6df1f09dc8b73200b6ac36
wesleyOnLucy <wesley@heungs.com>
2015-07-09 22:43:42 -0400

    Fixed navbar background being overriden by black colour. Fixed short images not filling the background and leaving white space. Fixed bridge.jpg not displaying properly. Started work on a page footer.

--- js/header.js
+++ js/header.js
@@ -5,10 +5,14 @@ $(function () {
         /* Set the active item on the navbar to to the one specified */
         $("#" + $("header").attr("active")).attr("class", "active");
     });
-    /* Fix anchors */
+    /* Fix anchors to account for the header */
     var shiftWindow = function () { scrollBy(0, -50); };
     window.onhashchange = shiftWindow;
     // Alternative method of above
     // window.addEventListener("hashchange", shiftWindow);
     if (window.location.hash) shiftWindow();
+
+
+    /* Load the footer HTML */
+    $("#page-footer").load("/footer.html");
 });

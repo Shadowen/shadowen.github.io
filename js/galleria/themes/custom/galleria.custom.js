a74145fcb805290b67e6869a8bce0c81073d0cb3
wesleyOnLucy <wesley@heungs.com>
2015-07-20 14:05:08 -0400

    Galleria theme modifications: Changed close button to FontAwesome's glyphicon. Simplified behaviour of info box (default open and closing). Fullscreen button behaviour simplified (css, rather than jquery). Counter moved to top.

--- js/galleria/themes/custom/galleria.custom.js
+++ js/galleria/themes/custom/galleria.custom.js
@@ -28,12 +28,13 @@
             var gallery = this;
             Galleria.requires(1.4, 'This theme requires Galleria 1.4 or later');
 
+            // Move the info element into the stage
+            gallery.$('info').detach().appendTo(gallery.$('stage'));
             // add some elements
             gallery.addElement('info-link', 'info-close');
             gallery.append({
                 'info': ['info-link', 'info-close']
             });
-
             // cache some stuff
             var info = gallery.$('info-link,info-close,info-text'),
             touch = Galleria.TOUCH;
@@ -42,11 +43,6 @@
             gallery.addElement('fscr');
             gallery.$('fscr').click(function () {
                 gallery.toggleFullscreen();
-            });
-            gallery.$('fscr').hover(function () {
-                $(this).animate({ opacity: 1 }, { duration: 100, queue: false });
-            }, function () {
-                $(this).animate({ opacity: 0.6 }, { duration: 100, queue: false });
             });
             gallery.appendChild('stage', 'fscr');
 
@@ -58,7 +54,6 @@
                 gallery.addIdleState(gallery.get('image-nav-left'), { left: -50 });
                 gallery.addIdleState(gallery.get('image-nav-right'), { right: -50 });
                 gallery.addIdleState(gallery.get('counter'), { opacity: 0 });
-                gallery.addIdleState(gallery.get('fscr'), { opacity: 0 });
             }
 
             // toggle info
@@ -66,29 +61,27 @@
                 info.bind('click:fast', function () {
                     info.toggle();
                 });
-                // Show info by default
-                info.toggle();
             } else {
                 info.show();
                 this.$('info-link, info-close').hide();
             }
 
             // bind some stuff
-            gallery.bind('thumbnail', function (e) {
+            this.bind('thumbnail', function (e) {
                 if (!touch) {
                     // fade thumbnails
                     $(e.thumbTarget).css('opacity', 0.6).parent().hover(function () {
-                        $(gallery).not('.active').children().stop().fadeTo(100, 1);
+                        $(this).not('.active').children().stop().fadeTo(100, 1);
                     }, function () {
-                        $(gallery).not('.active').children().stop().fadeTo(400, 0.6);
+                        $(this).not('.active').children().stop().fadeTo(400, 0.6);
                     });
-                    // Active thumbnail is bright
+
                     if (e.index === this.getIndex()) {
                         $(e.thumbTarget).css('opacity', 1);
                     }
                 } else {
-                    $(e.thumbTarget).css('opacity', gallery.getIndex() ? 1 : 0.6).bind('click:fast', function () {
-                        $(gallery).css('opacity', 1).parent().siblings().children().css('opacity', 0.6);
+                    $(e.thumbTarget).css('opacity', this.getIndex() ? 1 : 0.6).bind('click:fast', function () {
+                        $(this).css('opacity', 1).parent().siblings().children().css('opacity', 0.6);
                     });
                 }
             });
@@ -96,13 +89,12 @@
             var activate = function (e) {
                 $(e.thumbTarget).css('opacity', 1).parent().siblings().children().css('opacity', 0.6);
             };
-
             gallery.bind('loadstart', function (e) {
                 if (!e.cached) {
                     gallery.$('loader').show().fadeTo(200, 0.4);
                 }
                 window.setTimeout(function () {
-                    activate(e);
+                    activate(e)
                 }, touch ? 300 : 0);
                 gallery.$('info').toggle(gallery.hasInfo());
             });
@@ -110,13 +102,20 @@
             gallery.bind('loadfinish', function (e) {
                 gallery.$('loader').fadeOut(200);
             });
+            // Idle
+            gallery.bind('idle_enter', function () {
+                gallery.$('fscr').fadeOut();
+            });
+            gallery.bind('idle_exit', function () {
+                gallery.$('fscr').fadeIn();
+            });
             // Enter and exit fullscreen
             gallery.bind('fullscreen_enter', function () {
                 gallery.$('fscr').addClass('active');
             });
             gallery.bind('fullscreen_exit', function () {
                 gallery.$('fscr').removeClass('active');
-            })
+            });
         }
     });
 

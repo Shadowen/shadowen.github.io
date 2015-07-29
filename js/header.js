97c38433635cbc998c1dd52fddd0d56e1abfa275
wesleyOnLucy <wesley@heungs.com>
2015-07-29 14:10:05 -0400

    Updated Schedule Scraper to use raw.githack.com

--- js/header.js
+++ js/header.js
@@ -8,8 +8,6 @@ $(function () {
     /* Fix anchors to account for the header */
     var shiftWindow = function () { scrollBy(0, -50); };
     window.onhashchange = shiftWindow;
-    // Alternative method of above
-    // window.addEventListener("hashchange", shiftWindow);
     if (window.location.hash) shiftWindow();
 
 
@@ -17,21 +15,15 @@ $(function () {
     $("#page-footer").load("/footer.html");
 
     // Allows .selectable elements to be selected in a single click.
-    //$('.selectable').hover(function () { }, function () { });
-    //    background-color: rgba(255,255,255,0.6);
-    //    color: rgb(0,0,0);
-    //    content: 'Click to select text';
     // http://keestalkstech.com/2014/04/click-to-select-all-on-the-pre-element/
     $('.selectable').click(function () {
-        //        if (this.select) {
-        //            console.log('select');
-        //            this.select();
-        //        } else if (document.selection) {
-        //            console.log('document');
-        //            var range = document.body.createTextRange();
-        //            range.moveToElementText(this);
-        //            range.select();
-        //        } else
+        // if (this.select) {
+        //      this.select();
+        // } else if (document.selection) {
+        //      var range = document.body.createTextRange();
+        //      range.moveToElementText(this);
+        //      range.select();
+        // } else
         if (window.getSelection) {
             var range = document.createRange();
             range.selectNodeContents(this);
